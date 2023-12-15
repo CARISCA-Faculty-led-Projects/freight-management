eb9a6f3c42507f4e6251cae73e2a9db788a96cb6
janim2 <iamjesse75@gmail.com>
2023-12-15 23:28:13 +0000

    add broker and broker list added

---
+++ resources/views/partials/modals/add_broker.blade.php
@@ -0,0 +1,157 @@
+<!--begin::Modal - Broker - Add-->
+<div class="modal fade" id="kt_modal_add_customer" tabindex="-1" aria-hidden="true">
+    <!--begin::Modal dialog-->
+    <div class="modal-dialog modal-dialog-centered mw-650px">
+        <!--begin::Modal content-->
+        <div class="modal-content">
+            <!--begin::Form-->
+            <form class="form" action="#" id="kt_modal_add_customer_form" data-kt-redirect="/apps/customers/list">
+                <!--begin::Modal header-->
+                <div class="modal-header" id="kt_modal_add_customer_header">
+                    <!--begin::Modal title-->
+                    <h2 class="fw-bold">Add a Broker</h2>
+                    <!--end::Modal title-->
+                    <!--begin::Close-->
+                    <div id="kt_modal_add_customer_close" class="btn btn-icon btn-sm btn-active-icon-primary">
+                        <i class="ki-duotone ki-cross fs-1">
+                            <span class="path1"></span>
+                            <span class="path2"></span>
+                        </i>
+                    </div>
+                    <!--end::Close-->
+                </div>
+                <!--end::Modal header-->
+                <!--begin::Modal body-->
+                <div class="modal-body py-10 px-lg-17">
+                    <!--begin::Scroll-->
+                    <div class="scroll-y me-n7 pe-7" id="kt_modal_add_customer_scroll" data-kt-scroll="true"
+                        data-kt-scroll-activate="{default: false, lg: true}" data-kt-scroll-max-height="auto"
+                        data-kt-scroll-dependencies="#kt_modal_add_customer_header"
+                        data-kt-scroll-wrappers="#kt_modal_add_customer_scroll" data-kt-scroll-offset="300px">
+
+                        <!--begin::Input group-->
+                        <div class="d-flex flex-column mb-7 fv-row">
+                            <!--begin::Label-->
+                            <label class="fs-6 fw-semibold mb-2">
+                                <span class="required">Organization</span>
+                                <span class="ms-1" data-bs-toggle="tooltip" title="Country of origination">
+                                    <i class="ki-duotone ki-information fs-7">
+                                        <span class="path1"></span>
+                                        <span class="path2"></span>
+                                        <span class="path3"></span>
+                                    </i>
+                                </span>
+                            </label>
+                            <!--end::Label-->
+                            <!--begin::Input-->
+                            <select name="country" aria-label="Select a Country" data-control="select2"
+                                data-placeholder="Select an organization" data-dropdown-parent="#kt_modal_add_customer"
+                                class="form-select form-select-solid fw-bold">
+                                <option value="">Select an organization...</option>
+                                <option value="AF">Individual</option>
+                                <option value="AF">	Jess Fleet Mangement Inc</option>
+                                
+                            </select>
+                            <!--end::Input-->
+                        </div>
+                        <!--end::Input group-->
+                        <!--begin::Input group-->
+                        <div class="fv-row mb-7">
+                            <!--begin::Label-->
+                            <label class="required fs-6 fw-semibold mb-2">Name</label>
+                            <!--end::Label-->
+                            <!--begin::Input-->
+                            <input type="text" class="form-control form-control-solid" placeholder="" name="name"
+                                value="Sean Bean" />
+                            <!--end::Input-->
+                        </div>
+                        <!--end::Input group-->
+                        <!--begin::Input group-->
+                        <div class="fv-row mb-7">
+                            <!--begin::Label-->
+                            <label class="fs-6 fw-semibold mb-2">
+                                <span class="required">Phone</span>
+                                <span class="ms-1" data-bs-toggle="tooltip" title="Email address must be active">
+                                    <i class="ki-duotone ki-information fs-7">
+                                        <span class="path1"></span>
+                                        <span class="path2"></span>
+                                        <span class="path3"></span>
+                                    </i>
+                                </span>
+                            </label>
+                            <!--end::Label-->
+                            <!--begin::Input-->
+                            <input type="email" class="form-control form-control-solid" placeholder="" name="email"
+                                value="0265555454" />
+                            <!--end::Input-->
+                        </div>
+                        <!--end::Input group-->
+                        <!--begin::Input group-->
+                        <div class="fv-row mb-7">
+                            <!--begin::Label-->
+                            <label class="fs-6 fw-semibold mb-2">
+                                <span class="required">Email</span>
+                                <span class="ms-1" data-bs-toggle="tooltip" title="Email address must be active">
+                                    <i class="ki-duotone ki-information fs-7">
+                                        <span class="path1"></span>
+                                        <span class="path2"></span>
+                                        <span class="path3"></span>
+                                    </i>
+                                </span>
+                            </label>
+                            <!--end::Label-->
+                            <!--begin::Input-->
+                            <input type="email" class="form-control form-control-solid" placeholder="" name="email"
+                                value="sean@dellito.com" />
+                            <!--end::Input-->
+                        </div>
+                        <!--end::Input group-->
+                          <!--begin::Input group-->
+                          <div class="d-flex flex-column mb-7 fv-row">
+                            <!--begin::Label-->
+                            <label class="fs-6 fw-semibold mb-2">
+                                <span class="required">Status</span>
+                                <span class="ms-1" data-bs-toggle="tooltip" title="Country of origination">
+                                    <i class="ki-duotone ki-information fs-7">
+                                        <span class="path1"></span>
+                                        <span class="path2"></span>
+                                        <span class="path3"></span>
+                                    </i>
+                                </span>
+                            </label>
+                            <!--end::Label-->
+                            <!--begin::Input-->
+                            <select name="country" aria-label="Select a Country" data-control="select2"
+                                data-placeholder="Select a status" data-dropdown-parent="#kt_modal_add_customer"
+                                class="form-select form-select-solid fw-bold">
+                                <option value="P" selected='selected' default='default'>Pending</option>
+                                <option value="AF">Approved</option>
+                                
+                            </select>
+                            <!--end::Input-->
+                        </div>
+                        <!--end::Input group-->
+                    </div>
+                    <!--end::Scroll-->
+                </div>
+                <!--end::Modal body-->
+                <!--begin::Modal footer-->
+                <div class="modal-footer flex-center">
+                    <!--begin::Button-->
+                    <button type="reset" id="kt_modal_add_customer_cancel" class="btn btn-light me-3">Discard</button>
+                    <!--end::Button-->
+                    <!--begin::Button-->
+                    <button type="submit" id="kt_modal_add_customer_submit" class="btn btn-primary">
+                        <span class="indicator-label">Submit</span>
+                        <span class="indicator-progress">Please wait...
+                            <span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
+                    </button>
+                    <!--end::Button-->
+                </div>
+                <!--end::Modal footer-->
+            </form>
+            <!--end::Form-->
+        </div>
+    </div>
+</div>
+<!--end::Modal - Broker - Add-->
