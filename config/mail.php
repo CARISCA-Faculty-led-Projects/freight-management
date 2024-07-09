be3fb9ac1d5a551676732494013b6f4b05b4e4b8
janim2 <iamjesse75@gmail.com>
2024-07-09 15:20:20 +0000

    testing the auto deploy

--- config/mail.php
+++ config/mail.php
@@ -36,14 +36,14 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-            'url' => env('MAIL_URL'),
             'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
             'port' => env('MAIL_PORT', 587),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
-            'local_domain' => env('MAIL_EHLO_DOMAIN'),
+            'auth_mode' => null,
+            'verify_peer' => false,
         ],
 
         'ses' => [
@@ -99,7 +99,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'default@example.com'),
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
